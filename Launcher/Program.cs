70aac1321532d9fe692ae8b16e3db0d5c509cc1d
pavel.zybenkov <pavel.zybenkov@antilatency.com>
2023-01-25 19:07:26 +0300

    Initial implementation, with empty test project

---
+++ Launcher/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Program
+{
+    public static async Task Main()
+    {
+        //var targetProjectPath = Path.Combine(ProjectDirectory.Path, "..\\TestProject\\StaticSharpSourceGenerator.TestProject.csproj");
+        var targetProjectPath = Path.Combine(ProjectDirectory.Path, "..\\..\\StaticSharp\\StaticSharpDemo\\StaticSharpDemo.csproj");
+        var outputPath = Path.Combine(Path.GetDirectoryName(targetProjectPath), $".generated/{typeof(StaticSharpSourceGenerator.StaticSharpSourceGenerator).FullName}");
+        outputPath += "_detached";
+        await RoslynSourceGeneratorLauncher.RoslynSourceGeneratorLauncher.Launch(new StaticSharpSourceGenerator.StaticSharpSourceGenerator(), targetProjectPath, outputPath);
+    }
+}
